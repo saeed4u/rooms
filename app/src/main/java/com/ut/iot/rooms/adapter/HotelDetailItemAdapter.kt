b469e5aeac65efc92ff68ded1faece1b6374ef8a
Finnair <admin@Finnairs-MacBook-Pro.local>
2019-12-05 14:54:10 +0200

    Added hotel detail activity

--- app/src/main/java/com/ut/iot/rooms/adapter/HotelDetailItemAdapter.kt
+++ app/src/main/java/com/ut/iot/rooms/adapter/HotelDetailItemAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ut.iot.rooms.R
 import com.ut.iot.rooms.adapter.viewholder.HotelDetailItemViewHolder
 import com.ut.iot.rooms.data.model.HotelDetail
+import com.ut.iot.rooms.data.model.HotelDetailType
 
 /**
  * Created by Saeed on 02/12/2019.
@@ -14,13 +15,36 @@ import com.ut.iot.rooms.data.model.HotelDetail
 class HotelDetailItemAdapter(private val items: List<HotelDetail>) :
     RecyclerView.Adapter<HotelDetailItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HotelDetailItemViewHolder {
-        return HotelDetailItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.hotel_detail_item_ui,
-                parent,
-                false
-            )
-        )
+        return when(viewType){
+            HotelDetailType.TITLE.ordinal -> HotelDetailItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.hotel_detail_item_ui,
+                    parent,
+                    false
+                ))
+            HotelDetailType.PRICE.ordinal -> HotelDetailItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.hotel_detail_price_item,
+                    parent,
+                    false
+                ))
+            HotelDetailType.RATING.ordinal -> HotelDetailItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.rating_item_ui,
+                    parent,
+                    false
+                ))
+            else -> HotelDetailItemViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.hotel_detail_item_ui_items,
+                    parent,
+                    false
+                ))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].hotelDetailType.ordinal
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/ut/iot/rooms/adapter/viewholder/HotelDetailItemViewHolder.kt
+++ app/src/main/java/com/ut/iot/rooms/adapter/viewholder/HotelDetailItemViewHolder.kt
@@ -1,19 +1,32 @@
 package com.ut.iot.rooms.adapter.viewholder
 
 
+import android.annotation.SuppressLint
 import android.view.View
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ut.iot.rooms.R
 import com.ut.iot.rooms.data.model.HotelDetail
-import kotlinx.android.synthetic.main.hotel_detail_item_ui.view.*
+import com.ut.iot.rooms.data.model.HotelDetailType
+import java.text.DecimalFormat
 
 /**
  * Created by Saeed on 02/12/2019.
  */
 class HotelDetailItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
 
+    @SuppressLint("SetTextI18n")
     fun bind(item: HotelDetail){
-        view.item_title.text = item.title
-        view.item_value.text = item.value
+        when(item.hotelDetailType){
+            HotelDetailType.RATING -> {
+                view.findViewById<RatingBar>(R.id.hotel_rating).rating = item.value.toFloat()
+            }
+            HotelDetailType.TITLE, HotelDetailType.ROOM -> view.findViewById<TextView>(R.id.item).text = item.value
+            HotelDetailType.PRICE ->{
+                    view.findViewById<TextView>(R.id.pricing).text = "USD ${DecimalFormat("0.00").format(item.value.toDouble())} "
+            }
+        }
     }
 
 }
